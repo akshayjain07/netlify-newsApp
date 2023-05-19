0cf2b188c1154d194daa1fb10dd3d9d3aa73ba89
akshayjain07 <akshay.070603@gmail.com>
2023-05-19 07:34:02 +0530

    lecture 35

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl, author, date} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className="container my-3">
         <div className="card">
-        {/* <div className="card" style={{width: "21vw"}}> */}
           <img src={!imageUrl?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx6_zG-A23xeULPLNKhqDZiZrMYRdFj-hDNw&usqp=CAU": imageUrl} className="card-img-top" alt="..."/>
-          {/* <img src={!imageUrl?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx6_zG-A23xeULPLNKhqDZiZrMYRdFj-hDNw&usqp=CAU": imageUrl} style={{height:"21vh", width:"21vw"}} className="card-img-top" alt="..."/> */}
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}} >{source}</span></h5>
             <p className="card-text">{description}...</p>
-            <p className="card-text"> <small className="text-muted">By {author} on {date}</small> </p>
+            <p className="card-text"> <small className="text-muted">By {!author?"Akshay Jain":author} on {new Date(date).toGMTString()}</small> </p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More !!</a>
           </div>
         </div>
